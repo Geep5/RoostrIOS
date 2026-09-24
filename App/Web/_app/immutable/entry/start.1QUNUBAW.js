a9697ef5d48a5a1c313e713e278b831e6e2c2188
Grant Farwell <grantgfarwell@gmail.com>
2026-09-23 22:21:02 -0700

    Engine + web bundle: space default retired; setup always shows the computer picker

---
+++ App/Web/_app/immutable/entry/start.1QUNUBAW.js
@@ -0,0 +1,1 @@
+import{o as e,r as t}from"../chunks/CY0b3m9T.js";export{e as load_css,t as start};
--- App/Web/_app/immutable/nodes/1.Dhony6gL.js
+++
@@ -1,1 +0,0 @@
-import{$ as e,K as t,M as n,P as r,Q as i,Z as a,j as o,lt as s,mt as c,ut as l}from"../chunks/Cg2ytOsU.js";import"../chunks/xihTtKlq.js";import{t as u}from"../chunks/BP4LraEb.js";var d=r(`<h1> </h1> <p> </p>`,1);function f(r,f){l(f,!0);var p=d(),m=i(p),h=a(m,!0);c(m);var g=e(m,2),_=a(g,!0);c(g),t(()=>{o(h,u.status),o(_,u.error?.message)}),n(r,p),s()}export{f as component};
